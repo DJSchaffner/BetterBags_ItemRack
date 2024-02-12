e24dc98d2a28b4a1007f1164b33678316691d688
DJSchaffner <jonas.wilkens@outlook.com>
2024-02-12 20:26:28 +0100

    minor change to deploy batch file

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Copying files...
-cp -r ../BetterBags_ItemRack "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns"
+cp -r "../BetterBags_ItemRack" "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns"
 echo Deployed
