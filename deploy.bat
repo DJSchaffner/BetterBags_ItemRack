3419b00cba38a1d84d9fa3507c3eb5c16fd8ea54
DJSchaffner <jonas.wilkens@outlook.com>
2024-02-18 13:43:28 +0100

    improved deployment process

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Copying files...
-cp -r "../BetterBags_ItemRack" "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns"
+xcopy "..\BetterBags_ItemRack" "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns" /exclude:exclude.txt /y
 echo Deployed
