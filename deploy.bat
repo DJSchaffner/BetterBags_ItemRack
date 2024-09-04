adf1ab30719e006dd4ecfc6d7e379e94474fb073
DJSchaffner <jonas.wilkens@outlook.com>
2024-09-04 22:48:22 +0200

    Use new game path for deployment

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Copying files...
-xcopy "." "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\BetterBags_ItemRack" /exclude:exclude.txt /y /i
+xcopy "." "D:\Games\World of Warcraft\_classic_era_\Interface\AddOns\BetterBags_ItemRack" /exclude:exclude.txt /y /i
 echo Deployed
